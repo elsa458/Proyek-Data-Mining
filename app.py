896f129e1bd701a61d71035110c85a576a8f502e
elsa458 <aelsa680@gmail.com>
2025-05-29 14:23:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,6 @@
 import streamlit as st
-from pages import 1_Halaman_Utama, 2_Tampilkan_Data, 3_Prediksi_Model
 
 st.set_page_config(page_title="Dashboard Gaya Hidup", layout="wide")
 
 st.title("Dashboard Data Gaya Hidup Mahasiswa dan Stres")
 st.markdown("Selamat datang di aplikasi analisis gaya hidup mahasiswa berdasarkan data yang telah dikumpulkan.")
-
-st.sidebar.title("Navigasi")
-page = st.sidebar.radio("Pilih halaman:", ["Halaman Utama", "Tampilkan Data", "Prediksi Model"])
-
-if page == "Halaman Utama":
-    1_Halaman_Utama.main()
-elif page == "Tampilkan Data":
-    2_Tampilkan_Data.main()
-elif page == "Prediksi Model":
-    3_Prediksi_Model.main()
-
