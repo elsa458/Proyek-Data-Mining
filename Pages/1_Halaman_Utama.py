2c3946299d9ad7534ba4ff08cd2ea903d107debd
elsa458 <aelsa680@gmail.com>
2025-05-29 13:57:42 +0700

    Delete Pages directory

--- Pages/1_Halaman_Utama.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Halaman Utama")
-st.markdown("Ini adalah halaman utama untuk dashboard analisis gaya hidup mahasiswa.")
-
--- Pages/3_Prediksi_Model.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("Prediksi Model")
-st.markdown("Halaman ini nantinya akan digunakan untuk memprediksi tingkat stres mahasiswa berdasarkan fitur gaya hidup.")
-
